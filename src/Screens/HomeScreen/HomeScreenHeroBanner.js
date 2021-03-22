40b412bfb3a79d4be886bbc9aa83f583f25341b0
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-03-22 17:52:54 +0530

    added 3rd banner image

--- src/Screens/HomeScreen/HomeScreenHeroBanner.js
+++ src/Screens/HomeScreen/HomeScreenHeroBanner.js
@@ -11,7 +11,16 @@ function HomeScreenHeroBanner() {
             <div className="hero-banner-two">
                 <img className="img-desktop" src="https://cdn.shopify.com/s/files/1/1367/5207/files/Dual_Gender_Home_Comforts_1440x.jpg?v=1615371666" alt="" />
                 <img className="img-mobile" src={imgTwo} alt=""/>
-        </div>
+            </div>
+            <div className="hero-banner-three">
+            <>
+                    <img src="https://cdn.shopify.com/s/files/1/1367/5207/files/07_01_Shop_mens_ad779213-0fed-41df-a3ab-2f5245a07264_1440x.jpg?v=1615371691" alt="" />
+                </>
+                <>
+                    <img className="yoga-banner" src="https://images.pexels.com/photos/3094215/pexels-photo-3094215.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
+                    </>
+            </div>
+            
         </div>
     )
 }
