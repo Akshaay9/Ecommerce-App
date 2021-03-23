db80f218e227c30fd481263be590241529860d67
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-03-23 11:02:25 +0530

    added hero image to screen2(mens new releases)

---
+++ src/Screens/MensNewDrop/Index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ScreenTwoPoster from './MensNewDropPoster'
+
+function Index() {
+    return (
+        <>
+            <ScreenTwoPoster/>
+        </>
+    )
+}
+
+export default Index
