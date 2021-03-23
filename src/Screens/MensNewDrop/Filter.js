9b80948278478fd9267659ddc9a35d3c2269a26f
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-03-23 13:52:05 +0530

    added filters

--- src/Screens/MensNewDrop/Filter.js
+++ src/Screens/MensNewDrop/Filter.js
@@ -4,12 +4,13 @@ function Filter() {
   const [sortByPrice, setSortByPrice] = useState("relevance");
   const [sortByRating, setSortByRating] = useState();
   const [sortByDelivery, setSortByDelivery] = useState();
-    const [priceRange, setPriceRange] = useState(500);
-    // 
-    const [showPriceSortFilter,setShowPriceSortFilter]=useState(false)
-    const [showStarSortFilter,setStarPriceSortFilter]=useState(false)
-    const [showDeliverySortFilter,setDeliveryPriceSortFilter]=useState(false)
-    
+  const [priceRange, setPriceRange] = useState(500);
+  //
+  const [showPriceSortFilter, setShowPriceSortFilter] = useState(false);
+  const [showStarSortFilter, setStarPriceSortFilter] = useState(false);
+  const [showDeliverySortFilter, setDeliveryPriceSortFilter] = useState(false);
+  const [showDeliverySortFilter, setDeliveryPriceSortFilter] = useState(false);
+
   return (
     <div className="filter">
       <h3 className="filter-heading">Filters : </h3>
@@ -56,93 +57,93 @@ function Filter() {
         <h3 className="filter-heading-sort-rating">
           Sort By Rating{" "}
           <i class="fas fa-chevron-down" style={{ marginTop: "3px" }}></i>{" "}
-              </h3>
-              <div className="filter-heading-sort-rating-container">
-        <li>
-          {" "}
-          <label htmlFor="">4 Star and above</label>{" "}
-          <input
-            type="radio"
-            name="star"
-            checked={sortByRating == "fourStar" ? true : false}
-            onClick={() => setSortByRating("fourStar")}
-          />
-        </li>
-        <li>
-          {" "}
-          <label htmlFor="">3 Star and above</label>{" "}
-          <input
-            type="radio"
-            name="star"
-            checked={sortByRating == "threeStar" ? true : false}
-            onClick={() => setSortByRating("threeStar")}
-          />
-        </li>
-        <li>
-          {" "}
-          <label htmlFor="">2 Star and above</label>{" "}
-          <input
-            type="radio"
-            name="star"
-            checked={sortByRating == "twoStar" ? true : false}
-            onClick={() => setSortByRating("twoStar")}
-          />
-        </li>
-        <li>
-          {" "}
-          <label htmlFor="">1 Star and above</label>{" "}
-          <input
-            type="radio"
-            name="star"
-            checked={sortByRating == "oneStar" ? true : false}
-            onClick={() => setSortByRating("oneStar")}
-          />
-                  </li>
-                  </div>
+        </h3>
+        <div className="filter-heading-sort-rating-container">
+          <li>
+            {" "}
+            <label htmlFor="">4 Star and above</label>{" "}
+            <input
+              type="radio"
+              name="star"
+              checked={sortByRating == "fourStar" ? true : false}
+              onClick={() => setSortByRating("fourStar")}
+            />
+          </li>
+          <li>
+            {" "}
+            <label htmlFor="">3 Star and above</label>{" "}
+            <input
+              type="radio"
+              name="star"
+              checked={sortByRating == "threeStar" ? true : false}
+              onClick={() => setSortByRating("threeStar")}
+            />
+          </li>
+          <li>
+            {" "}
+            <label htmlFor="">2 Star and above</label>{" "}
+            <input
+              type="radio"
+              name="star"
+              checked={sortByRating == "twoStar" ? true : false}
+              onClick={() => setSortByRating("twoStar")}
+            />
+          </li>
+          <li>
+            {" "}
+            <label htmlFor="">1 Star and above</label>{" "}
+            <input
+              type="radio"
+              name="star"
+              checked={sortByRating == "oneStar" ? true : false}
+              onClick={() => setSortByRating("oneStar")}
+            />
+          </li>
+        </div>
       </ul>
       <ul>
         <h3 className="filter-heading-sort-delivery">
           Product Delivery{" "}
           <i class="fas fa-chevron-down" style={{ marginTop: "3px" }}></i>
-              </h3>
-              <div className="filter-heading-sort-delivery-container">
-        <li>
-          {" "}
-          <label htmlFor="">Free Home delivery (1 week)</label>{" "}
-          <input
-            type="radio"
-            name="delivery"
-            checked={sortByDelivery == "freeHomeDelivery" ? true : false}
-            onClick={() => setSortByDelivery("freeHomeDelivery")}
-          />
-        </li>
-        <li>
-          {" "}
-          <label htmlFor="">
-            Express delivery in 3 days ( charges applicable)
-          </label>{" "}
-          <input
-            type="radio"
-            name="delivery"
-            checked={sortByDelivery == "expressDelivery" ? true : false}
-            onClick={() => setSortByDelivery("expressDelivery")}
-          />
-                  </li>
-                  </div>
+        </h3>
+        <div className="filter-heading-sort-delivery-container">
+          <li>
+            {" "}
+            <label htmlFor="">Free Home delivery (1 week)</label>{" "}
+            <input
+              type="radio"
+              name="delivery"
+              checked={sortByDelivery == "freeHomeDelivery" ? true : false}
+              onClick={() => setSortByDelivery("freeHomeDelivery")}
+            />
+          </li>
+          <li>
+            {" "}
+            <label htmlFor="">
+              Express delivery in 3 days ( charges applicable)
+            </label>{" "}
+            <input
+              type="radio"
+              name="delivery"
+              checked={sortByDelivery == "expressDelivery" ? true : false}
+              onClick={() => setSortByDelivery("expressDelivery")}
+            />
+          </li>
+        </div>
       </ul>
       <div className="priceRange">
         <h3 className="filter-heading-sort-priceRange">
           Price Range{" "}
           <i class="fas fa-chevron-down" style={{ marginTop: "3px" }}></i>{" "}
-              </h3>
-              <div className="filter-heading-sort-priceRange-contsiner">
-        <input
-          type="range"
-          min="100"
-          max="1000"
-          value={priceRange}
-          onChange={(e) => setPriceRange(e.target.value)}
-        />
+        </h3>
+        <div className="filter-heading-sort-priceRange-contsiner">
+          <input
+            type="range"
+            min="100"
+            max="1000"
+            value={priceRange}
+            onChange={(e) => setPriceRange(e.target.value)}
+          />
         </div>
       </div>
     </div>
