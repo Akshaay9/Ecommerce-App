29ae145da665eb88cd129875e9a63779c2f17225
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-03-23 10:01:37 +0530

    added button animation and changed the button size

--- src/Screens/HomeScreen/HomeScreenHeroBanner.js
+++ src/Screens/HomeScreen/HomeScreenHeroBanner.js
@@ -16,8 +16,9 @@ function HomeScreenHeroBanner() {
           <h1>New Drops</h1>
           <h3>In Adapt & Critical</h3>
           <div className="hero-banner-call-to-action-btns">
-            <button className="btn btn-one">Show Womens</button>
-            <button className="btn btn-two">Show mens</button>
+          
+            <button className="btn btn-homescreen-white">Show Womens</button>
+            <button className="btn btn-homescreen-grey">Show mens</button>
           </div>
         </div>
       </div>
@@ -34,8 +35,8 @@ function HomeScreenHeroBanner() {
           <h1>Home Zone</h1>
           <h3>Our Comfiest fits</h3>
           <div className="hero-banner-call-to-action-btns">
-            <button className="btn btn-one">Show Womens</button>
-            <button className="btn btn-two">Show mens</button>
+            <button className="btn btn-homescreen-white">Show Womens</button>
+            <button className="btn btn-homescreen-grey">Show mens</button>
           </div>
         </div>
       </div>
@@ -50,7 +51,7 @@ function HomeScreenHeroBanner() {
           <div className="hero-banner-call-to-action hero-three-one-CTA">
             <h1>Resistace Training</h1>
             <div className="hero-banner-call-to-action-btns">
-              <button className="btn hero-three-one-CTA-btn">Shop Now</button>
+              <button className="btn btn-homescreen-white">Shop Now</button>
             </div>
           </div>
         </>
@@ -59,11 +60,11 @@ function HomeScreenHeroBanner() {
             className="yoga-banner"
             src="https://images.pexels.com/photos/3094215/pexels-photo-3094215.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
             alt=""
-                  />
-                  <div className="hero-banner-call-to-action hero-three-two-CTA">
+          />
+          <div className="hero-banner-call-to-action hero-three-two-CTA">
             <h1>Yoga</h1>
             <div className="hero-banner-call-to-action-btns">
-              <button className="btn hero-three-two-CTA-btn">Shop Now</button>
+              <button className="btn btn-homescreen-grey">Shop Now</button>
             </div>
           </div>
         </>
