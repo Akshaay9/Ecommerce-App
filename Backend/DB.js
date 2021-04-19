8837004a7294031dddacafd9578715cca325e075
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-04-19 13:30:32 +0530

    Backedn bug fix:5

--- Backend/DB.js
+++ Backend/DB.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 import config from "config"
-const mongoURI = config.get("mongoURI")
+// const mongoURI = config.get("mongoURI")
 
 const connectToDatabase = async () => {
     try {
