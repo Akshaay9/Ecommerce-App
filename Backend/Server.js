149762025028e6f33e6bfafb226c37279aa9ccea
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-04-29 18:37:02 +0530

    fixed backend bug

--- Backend/Server.js
+++ Backend/Server.js
@@ -19,13 +19,6 @@ dotenv.config();
 const __dirname = path.resolve()
 
 connectToDatabase();
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '//build')))
-
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, '/', 'build', 'index.html'))
-//   )
-// } else {
   app.get('/', (req, res) => {
     res.send('API is running....')
   })
